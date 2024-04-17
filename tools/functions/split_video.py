7817663032fc307bf75d9180515c2536b57e8a35
Otis Alejandro <128675915+OtisAlejandro@users.noreply.github.com>
2024-04-17 13:04:08 -0500

    multiprocessing

--- tools/functions/split_video.py
+++ tools/functions/split_video.py
@@ -1,4 +1,5 @@
 import os
+import time
 import cv2
 
 from tools.utils.print_color import MessageType, print_color
@@ -10,12 +11,13 @@ def split_video(video_path, output_path, skip, verbose):
     return print_color("[ERR] There was an error while opening the video. Please try again and/or open a GitHub issue if this persists.", MessageType.ERROR.value)
   
   # vars
-  frame_count = 0;
-  frame_index = 0;
+  frame_count = 0
+  frame_index = 0
   video_fc = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
   video_fps = video.get(cv2.CAP_PROP_FPS)
   video_duration = video_fc / video_fps
   video_splitted_fc = video_fc / skip
+  start_time = time.time()
 
   # print info
   print_color(f"\n[INFO] HANDLING VIDEO {os.path.basename(video_path)}", MessageType.INFO.value)
@@ -41,4 +43,6 @@ def split_video(video_path, output_path, skip, verbose):
     frame_index+=1
   
   video.release()
-  print_color(f"[SUCC] Successfully wrote {video_splitted_fc:.0f} frames to \"{output_path}\"", MessageType.SUCCESS.value)
+  end_time = time.time() 
+  time_taken = end_time - start_time  
+  print_color(f"[SUCC] Successfully wrote {video_splitted_fc:.0f} frames to \"{output_path}\" in {time_taken:.2f} seconds.", MessageType.SUCCESS.value)
--- tools/handle_videos.py
+++ tools/handle_videos.py
@@ -1,15 +1,36 @@
+import multiprocessing
 import os
 from tools.utils.print_color import MessageType, print_color
 from tools.functions.split_video import split_video
 
+
+def process_video(video_path, output_path, skip, verbose):
+    split_video(video_path, output_path, skip, verbose)
+
+
 def handle_videos(input_path, output_path, skip, threshold, verbose):
+    processes = []
+
     for root, dirs, files in os.walk(input_path):
         for file in files:
             if file.endswith((".mkv", ".mp4")):
                 video_path = os.path.join(root, file)  # full path
                 filename = os.path.splitext(file)[0]  # get filename without extension
-                output_folder = os.path.join(output_path, filename)  # create output folder path
-                os.makedirs(output_folder, exist_ok=True)  # create output folder if not exists
-                print(output_path)
-                split_video(video_path, output_folder, skip, verbose)  # pass output folder as output path
+                output_folder = os.path.join(
+                    output_path, filename
+                )  # create output folder path
+                os.makedirs(
+                    output_folder, exist_ok=True
+                )  # create output folder if not exists
 
+                # Start a new process to handle each video
+                process = multiprocessing.Process(
+                    target=process_video,
+                    args=(video_path, output_folder, skip, verbose),
+                )
+                processes.append(process)
+                process.start()
+
+    # Wait for all processes to finish
+    for process in processes:
+        process.join()
